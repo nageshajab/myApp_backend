240f7cf4233d0c2acc4269d749472df24047482e
Nagesh Ajab <nageshajab@hotmail.com>
2025-08-18 17:24:48 +0530

    1

--- Models/Watchlist.cs
+++ Models/Watchlist.cs
@@ -21,55 +21,56 @@ namespace myazfunction.Models
     }
     public enum Status
     {
-        NotStarted,
-        InProgress,
-        Completed,
-        Cancelled
+        NotStarted=1,
+        InProgress=2,
+        Completed=3,
+        Cancelled=4
     }
     public enum WatchlistType
     {
-        Movie,
-        WebSeries,
-        Documentary,
-        Other
+        Movie=1,
+        WebSeries=2,
+        Documentary=3,
+        Other=4
     }
     public enum Language
     {
-        English,
-        Hindi,
-        Tamil,        
-        Malyalam,
-        Marathi,
-        Other
+        English=1,
+        Hindi=2,
+        Tamil=3,        
+        Malyalam=4,
+        Marathi=5,
+        Korean=6,
+        Other=7
     }
     public enum Genre
     {
-        Action,
-        Comedy,
-        Drama,
-        Horror,
-        Thriller,
-        Romance,
-        SciFi,
-        Documentary,
-        Other
+        Action=1,
+        Comedy=2,
+        Drama=3,
+        Horror=4,
+        Thriller=5,
+        Romance=6,
+        SciFi=7,
+        Documentary=8,
+        Other=9
     }
     public enum Rating
     {
-        OneStar,
-        TwoStars,
-        ThreeStars,
-        FourStars,
-        FiveStars
+        OneStar=1,
+        TwoStars=2,
+        ThreeStars=3,
+        FourStars=4,
+        FiveStars=5
     }
     public enum Ott
     {
-        Netflix,
-        Prime,
-        Hotstar,
-        SonyLiv,
-        Zee5,
-        YouTube,
-        Other
+        Netflix=1,
+        Prime=2,
+        Hotstar=3,
+        SonyLiv=4,
+        Zee5=5,
+        YouTube=6,
+        Other=7
     }
 }
